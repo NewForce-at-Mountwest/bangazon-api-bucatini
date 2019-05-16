9d40efb5974284a56983f1dd195c803ce8fe42ec
Newforce <thecreativefitz@gmail.com>
2019-05-16 14:25:00 -0400

    Finished Employee, ProductType, TrainingProgram class

---
+++ BangazonAPI/BangazonAPI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int DepartmentId { get; set; }
+        public bool IsSupervisor { get; set; }
+    
+    }
+}
