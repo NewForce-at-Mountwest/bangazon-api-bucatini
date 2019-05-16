dea6907166215af0fc99ad91f03327318c85535b
Russell Miller <bet2buy@gmail.com>
2019-05-16 14:16:31 -0400

    added computer and department models

---
+++ BangazonAPI/BangazonAPI/Models/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Computer
+    {
+        public int id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Make { get; set; }
+
+        public DateTime DecomissionDate { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
---
+++ BangazonAPI/BangazonAPI/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Department
+    {
+        public int id { get; set; }
+
+        public int Budget { get; set; }
+
+        public string Name { get; set; }
+    }
+}
